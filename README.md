bb60fe9f944d2fe4078d3be8fe6d1ccb23eaaee5
David Matson <dmmatson@github.com>
2023-12-18 09:32:17 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,59 @@
+# What is this?
+Given a URL to a "read" Goodreads bookshelf, this script will download the book covers and create a collage. 
+
+This was created out of personal desire to print and frame posters representing a year in reading. See [Examples](#examples) below.
+
+# How do I use it?
+
+### Requirements
+* Python 3
+
+Go to your "read" bookshelf on Goodreads. Order by "Date read" descending. Copy the URL. It should look something like this:
+```
+https://www.goodreads.com/review/list/58160628-emily-books-with-emily-fox-on-youtube?order=d&per_page=100&shelf=read&sort=date_read&utf8=%E2%9C%93page=1
+```
+
+Remove the page # from the end, but leave the `page=`. If it's not at the end, move it there. It should look like this:
+```
+https://www.goodreads.com/review/list/58160628-emily-books-with-emily-fox-on-youtube?order=d&per_page=100&shelf=read&sort=date_read&utf8=%E2%9C%93page=
+```
+
+Feed this URL into the script:
+
+```bash
+virtualenv -p python3 venv
+source venv/bin/activate
+pip install -r requirements.txt
+python3 main.py -u <url>
+```
+
+There's also this code...
+```python
+if date_read > datetime.now() - timedelta(days=1 * 365):
+```
+
+...that limits to books read in the last year. Adjust as needed.
+
+# Notes
+
+* The script might remove image(s) from the collage in order to make a rectangle
+* The output is relatively high DPI. That can be adjusted here: `plt.savefig("collage.jpg", dpi=500, bbox_inches="tight", pad_inches=0)` 
+
+# Examples
+
+### Raw Output
+
+![collage](https://github.com/dmmatson/gr-collage/blob/main/collage.jpg?raw=true)
+
+### Inspiration (with additional manual editing)
+![frame](https://github.com/dmmatson/gr-collage/blob/main/frame.png?raw=true)
+
+# Based On
+
+https://joshualoong.com/2019/02/05/Programmatically-Creating-a-500-Book-Cover-Collage/
+
+# Disclaimer
+
+This project is not affiliated with Goodreads or Amazon.
+
+I have no idea if this is against Goodreads' terms of service. Use at your own risk.
---
+++ main.py
@@ -0,0 +1,156 @@
+import argparse
+import cv2
+import glob
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+import requests
+import sys
+
+from bs4 import BeautifulSoup
+from datetime import datetime, timedelta
+from natsort import natsorted
+
+
+def user_agent():
+    agents = [
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.72 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10) AppleWebKit/600.1.25 (KHTML, like Gecko) Version/8.0 Safari/600.1.25",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:33.0) Gecko/20100101 Firefox/33.0",
+        "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/600.1.17 (KHTML, like Gecko) Version/7.1 Safari/537.85.10",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko",
+        "Mozilla/5.0 (Windows NT 6.3; WOW64; rv:33.0) Gecko/20100101 Firefox/33.0",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.104 Safari/537.36",
+    ]
+    return random.choice(agents)
+
+
+def find_largest_factors(even_number):
+    sqrt_num = int(even_number**0.5)
+
+    factor1 = 0
+    factor2 = 0
+
+    for i in range(sqrt_num, 1, -1):
+        if even_number % i == 0:
+            factor1 = i
+            factor2 = even_number // i
+            break
+
+    return factor1, factor2
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-u", "--url", help="URL to read shelf, with page= at the end")
+
+args = parser.parse_args()
+
+if not args.url:
+    parser.print_help(sys.stderr)
+    exit()
+
+# Get read book cover URLs
+page = 0
+covers = []
+done = False
+
+while not done:
+    page += 1
+
+    url = args.url + str(page)    
+    headers = {"User-Agent": user_agent()}
+    print(f"Getting page {url}")
+    response = requests.get(url, headers=headers)
+    html = response.content
+    soup = BeautifulSoup(html)
+
+    table = soup.find("table", {"id": "books"})
+    books = table.find_all("tr", {"class": "bookalike review"})
+
+    for book in books:
+        cover_url = (
+            book.find("img")["src"]
+            .replace("SY75", "")
+            .replace("SX50", "")
+            .replace("__", "")
+            .replace("..", ".")
+            .replace("._.", ".")
+        )
+        title = book.find("td", {"class": "field title"}).text.replace("\n", "").strip()
+
+        date_read = (
+            book.find("td", {"class": "date_read"})
+            .text.replace("date read", "")
+            .replace("not set", "")
+        )
+        date_read = list(filter(None, date_read.split("\n")))
+        date_read = date_read[0]
+        if len(date_read.replace(",", " ").split(" ")) < 3:
+            month, year = date_read.replace(",", " ").split(" ")
+            date_read = f"{month} 1, {year}"
+        date_read = datetime.strptime(date_read, "%b %d, %Y")
+
+        if "nophoto" in cover_url:
+            continue
+
+        if date_read > datetime.now() - timedelta(days=1 * 365):
+            print(title, date_read, cover_url)
+            covers.append(cover_url)
+        else:
+            done = True
+            break
+
+# Download covers
+print("\n")
+for i, cover in enumerate(covers):
+    print(f"Download progress: {round((float(i) / len(covers)) * 100)}%")
+    image = requests.get(cover).content
+    with open(f"image{i}.jpg", "wb") as handler:
+        handler.write(image)
+
+
+# Adjust number of images so we have a rectangle
+print("\n")
+image_paths = glob.glob("image*.jpg")
+image_paths = natsorted(image_paths)
+num_imgs = len(image_paths)
+width, height = find_largest_factors(num_imgs)
+while width < 6:
+    print(f"Adjust for rectangle, pruning {image_paths.pop()}")
+    num_imgs = len(image_paths)
+    width, height = find_largest_factors(num_imgs)
+
+# Create collage
+print("\n")
+print(f"Creating collage from {num_imgs} images")
+column_count = 1
+column_list = []
+column_matrix = []
+
+for path in image_paths:
+    img = cv2.imread(path, 1)
+    img = cv2.resize(img, (200, 300))
+
+    if column_count < height:
+        column_list.append(img)
+        column_count += 1
+    elif column_count == height:
+        column_list.append(img)
+        column = np.vstack(column_list)
+        column_matrix.append(column)
+        column_count = 1
+        column_list = []
+
+if len(column_list) > 0:
+    column = np.vstack(column_list)
+    column_matrix.append(column)
+
+collage = np.hstack(column_matrix)
+plt.figure(figsize=(60, 30))
+plt.axis("off")
+plt.imshow(collage[:, :, ::-1])
+plt.savefig("collage.jpg", dpi=500, bbox_inches="tight", pad_inches=0)
+plt.close()
